df84e12873ae37e863c6088a8d6b5aaf20e94153
parthacollekt <partha@collekt.ai>
2025-02-17 19:19:34 +0530

    added report

---
+++ config/config_helper.py
@@ -0,0 +1,7 @@
+api_urls = {
+    "create_campaign": "http://localhost:7070/campaigns",
+    "update_campaign": "http://localhost:7070/campaigns",
+    "get_campaign": "http://localhost:7070/campaigns",
+    "recipient_list": "http://localhost:7072/recipients/lists",
+    "email_template": "http://localhost:7071/email/templates"
+}
